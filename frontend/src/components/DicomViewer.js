eb1f0f9609c556e81fe52b1e0a3c520534dabc3c
Benja <benjargomezc@gmail.com>
2024-09-15 21:53:42 -0300

    se realiza la implementacion de funcion añadir imagen en el frontend

--- frontend/src/components/DicomViewer.js
+++ frontend/src/components/DicomViewer.js
@@ -7,7 +7,42 @@ import '../styles/styles.css'; // Importa tu archivo CSS personalizado
 
 const DicomViewer = () => {
   const [images, setImages] = useState([]);
+  const [patientId, setPatientId] = useState('');
   const [error, setError] = useState('');
+
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result); // Esto será el archivo en formato base64
+      };
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const anadirimagen = async (imageBase64, patientId) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/images', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          image: imageBase64,
+          patientId: patientId,
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Imagen y ID de paciente enviados correctamente');
+      } else {
+        console.error('Error al enviar la imagen y el ID de paciente');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
+  };
 
   const handleFileChange = async (e) => {
     const fileList = e.target.files;
@@ -23,8 +58,20 @@ const DicomViewer = () => {
         for (let i = 0; i < fileList.length; i++) {
           const file = fileList[i];
           if (file.name.endsWith('.dcm')) {
+            const arrayBuffer = await file.arrayBuffer();
+            const dataSet = dicomParser.parseDicom(new Uint8Array(arrayBuffer));
+
+            const patientId = dataSet.string('x00100020');
+            setPatientId(patientId); // Guardar el ID del paciente en el estado
+
             const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
             imageIds.push(imageId);
+
+            const imageBase64 = await convertToBase64(file)
+
+            // Llamar a la función para enviar la imagen y el ID al backend
+            await anadirimagen(imageBase64, patientId);
+
           }
         }
 
